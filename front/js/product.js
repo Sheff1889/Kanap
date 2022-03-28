575646ca262d5e60f6a7b9c965d30b1c2202d910
Sheff1889 <sebastian1889.r@gmx.at>
2022-03-28 14:14:27 +0200

    insert product details into product page

--- front/js/product.js
+++ front/js/product.js
@@ -32,7 +32,7 @@ fetchPromise.then((response) => {
   const id = products.map((products) => {
     return products._id
   }).join("\n");
-  // console.log(id);
+  //console.log(id);
 }); 
 
 
@@ -40,10 +40,84 @@ fetchPromise.then((response) => {
 
 
 
+
+
+
+
+
+
+const productTitle = document.querySelector('#title') ;
+const productId = getParameter('id');
+const fetchDescription = fetch(`http://localhost:3000/api/products/${productId}`);
+
+fetchDescription.then((response) => {
+  return response.json();
+})
+.then((product) => {
+  console.log(product);
  
+  productTitle.textContent =  product.productTitle;
+  
+}); 
 
 
 
+const api = 'http://localhost:3000/api/products/';
+
+
+
+
+fetch(api + productId)
+  .then((response) => response.json())
+  .then((data) => createCard(data));
+
+  function createCard(object) {
+
+    //function for getting image
+    function imageSofa() {
+      var frame = document.querySelector('.item__img');
+      var img = document.createElement("IMG");
+      img.src = ` ../images/logo.png`;
+      //img.width = "300";
+      frame.appendChild(img);
+    }
+    
+    imageSofa(); 
+
+
+    let name = document.getElementById('title');
+    name.innerText = object.name;
+    let price = document.getElementById('price');
+    price.innerText = object.price;
+
+    let description = document.getElementById('description');
+    description.innerText = object.description;
+    
+
+
+    
+
+
+
+  // chose color for sofa  
+
+    let optionsProduct = document.getElementById('colors');
+
+  // for loop with options (color)
+  for (let i = 0; i < object.colors.length; i++) {
+
+    let option = document.createElement('option');
+    option.innerText = object.colors[i];
+    optionsProduct.appendChild(option);
+  }
+
+  };
+
+
+
+
+
+  
 
 
 
