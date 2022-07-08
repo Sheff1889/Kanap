df777819bbebe389a92c3490bfc12d0e3450ee35
Sheff1889 <sebastian1889.r@gmx.at>
2022-07-08 09:39:35 +0200

    getting product elements and displaying the product items on the product page

---
+++ front/js/product.js
@@ -0,0 +1,48 @@
+const idProduct = new URL(window.location.href).searchParams.get("id");
+console.log(idProduct)
+
+
+//getting the product elements
+let imgProduct = document.querySelector(".item__img");
+let img = document.createElement("img");
+imgProduct.appendChild(img);
+let titleProduct = document.getElementById("title");
+let priceProduct = document.getElementById("price");
+let descriptionProduct = document.getElementById("description");
+let colorsProduct = document.getElementById("colors");
+
+
+
+// call function
+getProductArticle();
+
+//Displaying the items on the product page
+async function getProductArticle() {
+  //returns a promise
+  await fetch("http://localhost:3000/api/products/" + idProduct)
+  // add .then to get the data out of the promise +responds from the API
+ .then(response =>  {
+  // convert the response into JSON, that returns another promise
+  return response.json()
+ })
+ // get the data(JSON from the API) from the URL/idProducts
+ .then(product => {
+  //console.log(product)
+  title.innerHTML = `<h1>${product.name}</h1>`;
+  price.innerHTML = `${product.price}`;
+  description.innerHTML = `${product.description}`;
+  imgProduct.innerHTML=`<img src="${product.imageUrl}" alt="${product.altTxt}">`
+  img.setAttribute("src", product.imageUrl);
+  img.setAttribute("alt", product.altTxt);
+
+
+     for (let i=0; i < product.colors.length; i++) {
+         var color = product.colors[i]
+        colors.innerHTML += `<option value="${color}">${color}</option> `
+     }
+ })
+ // if there is an error
+ .catch((error) => {
+  console.error(error);
+ });
+}
